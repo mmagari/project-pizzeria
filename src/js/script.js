2514090cd681e124a5ba8b961521437a7484e8cb
Jarek.Mankiewicz <mmagari@wp.pl>
2023-06-05 16:15:51 +0200

    add widget counting functions

--- src/js/script.js
+++ src/js/script.js
@@ -62,8 +62,8 @@ const select = {
       //console.log('new Product: ', thisProduct);
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
-      thisProduct.processOrder();
-      
+      thisProduct.initAmountWidget();
+      thisProduct.processOrder(); 
     }
 
     renderInMenu(){
@@ -86,6 +86,7 @@ const select = {
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
       thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+      thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
     }
     
     initAccordion(){
@@ -172,8 +173,72 @@ const select = {
           }
         }
       }
+      //update if products amount is changed
+      price *= thisProduct.amountWidget.value;
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
+    }
+    initAmountWidget(){
+      const thisProduct = this;
+      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+      thisProduct.amountWidgetElem.addEventListener('updated', function(event){
+      event.preventDefault();
+      thisProduct.processOrder();
+      });
+    }
+  }
+
+  class AmountWidget{
+    constructor(element){
+      const thisWidget = this;
+      thisWidget.value = settings.amountWidget.defaultValue;
+      thisWidget.getElements(element);
+      thisWidget.setValue(thisWidget.input.value);
+      thisWidget.initActions();
+      console.log('AmountWidget: ', thisWidget);
+      console.log('constructor arguments: ', element);
+    }
+    getElements(element){
+      const thisWidget = this;
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    }
+    initActions(){
+      const thisWidget = this;
+      thisWidget.input.addEventListener('change', function(event){
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.input.value);
+      });
+      thisWidget.linkDecrease.addEventListener('click', function(event){
+        event.preventDefault();
+        const newValue = parseInt(thisWidget.input.value) - 1;
+        thisWidget.setValue(newValue);
+      });
+      thisWidget.linkIncrease.addEventListener('click', function(event){
+        event.preventDefault();
+        const newValue = parseInt(thisWidget.input.value) + 1;
+        thisWidget.setValue(newValue);
+      });
+    }
+    setValue(value){
+      const thisWidget = this;
+      
+      const newValue = parseInt(value);
+      console.log('nowa wartość', newValue);
+      if (thisWidget.value !== newValue && !isNaN(newValue) && newValue >= settings.amountWidget.defaultMin && newValue <= settings.amountWidget.defaultMax + 1){
+      /* TODO: Add validation */
+      thisWidget.value = newValue;
+      thisWidget.annouce();
+      }
+      thisWidget.input.value = thisWidget.value;
+    }
+    annouce(){
+      const thisWidget = this;
+
+      const event = new Event('updated');
+      thisWidget.element.dispatchEvent(event);
     }
   }
   
